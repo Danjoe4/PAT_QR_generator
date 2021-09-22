59ead435018977f852e0b6c6fc7a26378111d644
Danjoe4 <djbroderick4@gmail.com>
2021-09-21 21:06:48 -0400

    aes_encryption

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # coding=utf-8
-from flask import Flask, render_template, request, send_file
-#from cryptography.fernet import Fernet
-import zlib # for compression
+from flask import Flask, render_template, request, send_file, redirect
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64 
 import io
-
+import yaml
 
 
 app = Flask(__name__)
 
-######### make the qrcode ###########
+######### make the qrcode ##############
 import qrcode
 # for putting the logo in the QR code center
 from PIL import Image
@@ -20,25 +21,37 @@ basewidth = 100
 wpercent = (basewidth/float(logo.size[0]))
 hsize = int((float(logo.size[1])*float(wpercent)))
 logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+########################################
 
-##########################
+# a few more useful global CONSTANTS
+def load_globals():
+    with open("config.yaml") as f_stream:
+        config_file = yaml.load(f_stream, yaml.FullLoader)
 
-# a few more useful globals
+    global ENCRYPTION_KEY_1 # set the encryption key
+    salt = bytes(config_file["encryption_salt_1"], encoding='utf8')
+    password = bytes(config_file["encryption_password_1"], encoding='utf8')
+    ENCRYPTION_KEY_1 = derive_encryption_key(salt, password)
+    print("here is the encryption key:")
+    print(ENCRYPTION_KEY_1)
+
 BASE_URL = "http://www.vaultqr.com/send?" 
-
-
-
-
+###############################
 @app.route("/",methods=['GET'])
+def root():
+    return redirect("/make-product-code")
+
+
+@app.route("/make-product-code",methods=['GET'])
 def index():
-    """ the root page with the fillable form
+    """ the page with the fillable form
     """
-    return render_template("home.html")
+    return render_template("make_product_code.html")
 
 
 @app.route("/code", methods=["POST"])
 def code():
-    """ makes the QR code if it received a POST request from the root page
+    """ makes the QR code if it received a POST request from the form page
     """
     if request.method == 'POST':
         qr_img = make_qr(request.form['brand'], 
@@ -57,6 +70,8 @@ def make_qr(brand, product, model, serial):
     """ Takes the params and returns a base64 encoded image object
     """
     url = make_url(brand, product, model, serial)
+    print("the full url:")
+    print(url)
     
     # initialize, add URL to QRcode and generate
     QRcode = qrcode.QRCode(
@@ -83,37 +98,43 @@ def make_qr(brand, product, model, serial):
 
 def make_url(brand, product, model, serial):
     # HASH (add encryption) the params and create the url
-    ciphertext = obscure_params(f"{brand},{product},{model},{serial}".encode('utf-8'))
+    ciphertext = encrypt_params(f"{brand},{product},{model},{serial}".encode('utf-8'))
     url = BASE_URL + "v=" + str(ciphertext)[2:-1] # str cast adds b'...' to the cipher
     print("the url: " + url)
     return url
 
-def encrypt_params(params):
-    """ Encrypts the list of params: [brand,product,model,serial] and returns the 
-    encrypted string.
-    This algorithm produces a string that is too long, use a different one
+
+def encrypt_params(data: bytes) -> bytes:
+    """ aes128 the string
     """
-    f = Fernet(KEY)
-    token = f.encrypt(params)
+    print("the paramaters to be encoded: ")
+    print(data)
+    f = Fernet(ENCRYPTION_KEY_1)
+    token = f.encrypt(data)
+    print("encrypted paramter: ")
     print(token)
     return token
 
 
-def obscure_params(data: bytes) -> bytes:
-    """ A temporary solution to getting a smaller output. Converts to base64
-    then uses compression
+def derive_encryption_key(salt, password):
+    """ Uses our salt and password to derive the key
     """
-    print("the paramaters to be encoded: ")
-    print(data)
-    out = base64.urlsafe_b64encode(zlib.compress(data, 9))
-    print("encoded (base 64) paramter: ")
-    print(out)
-    return out
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=10000)
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    return key
 
 
 
 
 if __name__ == "__main__":
+    app.before_first_request(load_globals) 
     app.run(port=5000, debug=True)
-    #make_qr("Bose", "HEADPHONES", "6DBL", "SOMESERIAL2423567")
-   
+    
+    #load_globals()
+    #enc_str = encrypt_params("brand,product,model,serial".encode('utf-8'))
+    #make_qr("Bose", "HEADPHONES", "123456", "SOMESERIAL123")
+    #decrypt_params(enc_str)
